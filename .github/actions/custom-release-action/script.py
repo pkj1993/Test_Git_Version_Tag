d15fcce13683df74e914c33aa04fdcb7f642bba6
pkj1993 <140062112+pkj1993@users.noreply.github.com>
2023-08-09 00:25:52 +0530

    Update script.py

--- .github/actions/custom-release-action/script.py
+++ .github/actions/custom-release-action/script.py
@@ -1,6 +1,19 @@
 # /.github/actions/create-symmetric-tag/script.py
 import os
+import re
 from github import Github
+
+def increment_version(version, version_bump):
+    major, minor, patch = map(int, version.split('.'))
+    
+    if version_bump == 'Major':
+        new_version = f'{major + 1}.0.0'
+    elif version_bump == 'Minor':
+        new_version = f'{major}.{minor + 1}.0'
+    else:
+        new_version = f'{major}.{minor}.{patch + 1}'
+    
+    return new_version
 
 def create_symmetric_tag(repo, tag_name, commit_sha, tag_message):
     # Create a local tag
@@ -22,10 +35,12 @@ def main():
     repo = g.get_repo('pkj1993/Test_Git_Version_Tag')  # Replace with your repo details
 
     commit_sha = os.environ.get('GITHUB_SHA')
-    tag_name = os.environ.get('INPUT_TAG_NAME', 'v1.0.0')  # Default to v1.0.0 if not specified
-    tag_message = f"Version {tag_name}"
+    version_bump = os.environ.get('INPUT_VERSION_BUMP', 'Bugfix')  # Default to 'Bugfix'
+    current_version = '1.0.0'  # Get the current version from your project
 
-    create_symmetric_tag(repo, tag_name, commit_sha, tag_message)
+    next_version = increment_version(current_version, version_bump)
+
+    create_symmetric_tag(repo, next_version, commit_sha, f"Version {next_version}")
 
 if __name__ == '__main__':
     main()
